7e63b9f1964f1554569058de063095e7ad082340
surya <suryalucky98854@gmail.com>
2026-01-05 14:15:15 +0000

    k

--- backend/main.py
+++ backend/main.py
@@ -43,7 +43,10 @@ from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel, EmailStr
 from pymongo import MongoClient
 
-app = FastAPI()
+app = FastAPI(
+	docs_url="api/docs",
+	openapi_url="/api/openapi.json"
+)
 
 app.add_middleware(
     CORSMiddleware,
